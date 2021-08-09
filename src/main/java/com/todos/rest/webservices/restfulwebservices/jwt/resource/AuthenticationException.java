03975961dc39c9082c742d24a8ab569b6689e663
HTian1997 <htian1997@gmail.com>
2021-08-09 13:00:42 -0500

    upload Java files

---
+++ src/main/java/com/todos/rest/webservices/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.todos.rest.webservices.restfulwebservices.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'default', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'default', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'default', 'Learn Microservices', sysdate(), false);
